7ce23fcef14be00e16b40559764a4287407f227e
TharushaK01 <tharushakavinda01@gmail.com>
2024-08-09 07:43:32 +0530

    useState not working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+/* eslint-disable no-undef */
 import './App.css';
 
 function App() {
+
+  const [audioProgress, setAudioProgress] = useState(60)
+
+  const handleMusicProgressBar = () => {
+    setAudioProgress(e.target.value)
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <video src="./Videos/1.mp4"></video>
+      <div className="blackScreen"></div>
+      <div className="music-Container">
+        <p className='musicPlayer'>Music Player</p>
+        <p className='music-Head-Name'>Chasing</p>
+        <p className='music-Artist-Name'>Gangadara</p>
+        <img src="./Images/1.jpg" alt="song Avatar" id='songAvatar'/>
+        <div className="musicTimeDiv">
+          <p className='musicCurrentTime'>00 : 00</p>
+          <p className='musicTotalLength'>03 : 49</p>
+        </div>
+        <input type="range" name="musicProgressBar" className='musicProgressBar' value={audioProgress} onChange={handleMusicProgressBar}/>
+        <div className="musicControlers">
+          <i className='da-solid fa-backward'></i>
+          <i className='da-solid fa-circle-play'></i>
+          <i className='da-solid fa-forward'></i>
+        </div>
     </div>
+    </div>
+    </>
   );
 }
 
